215a6b876119fd8d3dcb4f9821ddddb33edb85ad
juti3000 <juangutierrezramirrez@gmail.com>
2024-03-15 20:52:21 +0100

    rasultado 1

--- test_front/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ test_front/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,17 @@
-package com.example.myapplication
-
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val RECORD_AUDIO_PERMISSION_CODE = 101
+    private val READ_EXTERNAL_STORAGE_PERMISSION_CODE = 102
+    private val WRITE_EXTERNAL_STORAGE_PERMISSION_CODE = 103
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,28 +26,72 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+
+        Handler().postDelayed({
+            if (arePermissionsGranted()) {
+                goToNextActivity()
+            } else {
+                requestPermissions()
+            }
+        }, 5000)
+    }
+
+    private fun arePermissionsGranted(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            RECORD_AUDIO_PERMISSION_CODE
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    }
+
+    private fun goToNextActivity() {
+        // Aquí puedes iniciar la siguiente actividad o realizar otras acciones según sea necesario
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            RECORD_AUDIO_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permiso de grabación de audio concedido
+                    // Puedes realizar las acciones necesarias aquí
+                    goToNextActivity()
+                } else {
+                    // Permiso de grabación de audio denegado
+                    // Informar al usuario sobre la importancia de este permiso
+                    // Aquí puedes mostrar un mensaje o tomar alguna otra acción
+                }
+            }
+            // Agregar casos para otros permisos si es necesario
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
